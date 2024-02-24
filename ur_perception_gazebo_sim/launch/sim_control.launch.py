6072a4e1d3b597a1c84710904c672ddb7c9464c8
X. Y <1023515576@qq.com>
2024-02-24 20:03:29 +0800

    add ur_perception_gazebo_sim

--- ur_perception_gazebo_sim/launch/sim_control.launch.py
+++ ur_perception_gazebo_sim/launch/sim_control.launch.py
@@ -6,36 +6,49 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, EmitEvent, ExecuteProcess, LogInfo, RegisterEventHandler, TimerAction
+from launch.conditions import IfCondition
+from launch.event_handlers import OnExecutionComplete, OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown
+from launch.events import Shutdown
+from launch.substitutions import (
+    EnvironmentVariable,
+    FindExecutable,
+    LaunchConfiguration,
+    LocalSubstitution,
+    PythonExpression,
+)
 
 kThisPackageName = "ur_perception_gazebo_sim"
 
 
 def generate_launch_description():
     launch_entities = []
-    launch_entities.append(
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory("ur_simulation_gazebo"), "launch/ur_sim_control.launch.py")]
-            ),
-            launch_arguments={
-                "description_package": "ur_perception_description",
-                "description_file": "ue5e_perception.urdf.xacro",
-                "launch_rviz": "false",
-            }.items(),
-        )
+
+    ur_sim_gazebo_launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory("ur_simulation_gazebo"), "launch/ur_sim_control.launch.py")]
+        ),
+        launch_arguments={
+            "description_package": "ur_perception_description",
+            "description_file": "ue5e_perception.urdf.xacro",
+            "launch_rviz": "false",
+        }.items(),
     )
+    launch_entities.append(ur_sim_gazebo_launch_file)
 
     # rviz
-    rviz_config_file = os.path.join(get_package_share_directory(kThisPackageName), "rviz", "view_robot_rgbd.rviz")
-    launch_entities.append(
-        Node(
-            package="rviz2",
-            executable="rviz2",
-            name="rviz2",
-            output="log",
-            arguments=["-d", rviz_config_file],
-            parameters=[{"use_sim_time": True}],
-        )
+    rviz_config_file = os.path.join(get_package_share_directory(kThisPackageName), "rviz", "view_robot_perception.rviz")
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        # output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[{"use_sim_time": True}],
     )
+    launch_entities.append(rviz_node)
 
     return LaunchDescription(launch_entities)
