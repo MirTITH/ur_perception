b4442244edc3d287d7e7fbb1743ad65bc4857194
X. Y <1023515576@qq.com>
2024-03-04 19:41:41 +0800

    delete unnecessary imports of sim_control.launch.py

--- ur_perception_gazebo_sim/launch/sim_control.launch.py
+++ ur_perception_gazebo_sim/launch/sim_control.launch.py
@@ -1,25 +1,11 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch_ros.actions import Node
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, EmitEvent, ExecuteProcess, LogInfo, RegisterEventHandler, TimerAction
-from launch.conditions import IfCondition
-from launch.event_handlers import OnExecutionComplete, OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown
-from launch.events import Shutdown
-from launch.substitutions import (
-    EnvironmentVariable,
-    FindExecutable,
-    LaunchConfiguration,
-    LocalSubstitution,
-    PythonExpression,
-)
 
 kThisPackageName = "ur_perception_gazebo_sim"
 
