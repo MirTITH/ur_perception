7e7a3eb7da715bda3579197e6fe93c998841e43f
X. Y <1023515576@qq.com>
2024-03-13 16:56:58 +0800

    重构 hdz_simplified：generated from solidworks

--- hdz_simplified/launch/view_robot.launch.py
+++ hdz_simplified/launch/view_robot.launch.py
@@ -1,57 +1,23 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
-from typing import Tuple, Dict, List
 
 kThisPackageName = "hdz_simplified"
 
 
 def generate_launch_description():
-    launch_entities = []
+    ld = LaunchDescription()
 
-    # robot_description_content = Command(
-    #     [
-    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
-    #         " ",
-    #         PathJoinSubstitution([FindPackageShare(kThisPackageName), "urdf", "hdz_simplified.urdf"]),
-    #     ]
-    # )
-
-    urdf_file = os.path.join(get_package_share_directory(kThisPackageName), "urdf", "hdz_simplified.urdf")
-
-    with open(urdf_file, "r") as f:
-        robot_description_content = f.read()
-
-    launch_entities.append(
-        Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            output="both",
-            parameters=[{"robot_description": robot_description_content}],
+    ld.add_action(
+        IncludeLaunchDescription(
+            PathJoinSubstitution([FindPackageShare("urdf_launch"), "launch", "display.launch.py"]),
+            launch_arguments={
+                "urdf_package": kThisPackageName,
+                "urdf_package_path": PathJoinSubstitution(["urdf", "hdz_simplified.urdf"]),
+                "rviz_config": PathJoinSubstitution([FindPackageShare(kThisPackageName), "config", "view_robot.rviz"]),
+            }.items(),
         )
     )
 
-    launch_entities.append(
-        Node(
-            package="joint_state_publisher_gui",
-            executable="joint_state_publisher_gui",
-        )
-    )
-
-    rviz_config_file = PathJoinSubstitution([FindPackageShare(kThisPackageName), "config", "view_robot.rviz"])
-    launch_entities.append(
-        Node(
-            package="rviz2",
-            executable="rviz2",
-            name="rviz2",
-            output="log",
-            arguments=["-d", rviz_config_file],
-        )
-    )
-
-    return LaunchDescription(launch_entities)
+    return ld
